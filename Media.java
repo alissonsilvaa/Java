46fb68ceaee3ba515c1922deaefb0148cda7a547
Alisson Silva <78627552+alissonsilvaa@users.noreply.github.com>
2023-11-22 21:42:29 -0300

    Add files via upload

---
+++ Media.java
@@ -0,0 +1,17 @@
+class Media{
+	public static void main(String[]args){
+		
+		float numeroX = 7.9f;
+		float numeroY = 15.7f;
+
+		float numeroR = (numeroX + numeroY) / 2;
+
+	System.out.println("Media: " + numeroR);
+	
+
+	}
+
+
+
+
+}
