84d9d99ba230706963bf214b09295399a2642ad6
Alisson Silva <78627552+alissonsilvaa@users.noreply.github.com>
2023-12-05 18:11:49 -0300

    Update Administrativo.java

--- EX06/Administrativo.java
+++ EX06/Administrativo.java
@@ -1,14 +1,19 @@
 package EX06;
 
-public class Administrativo extends Funcionario {
+public class Administrativo extends Assistente{
     private boolean isDia;
+
+    public boolean isDia() {
+        return isDia;
+    }
+
+    public void setDia(boolean dia) {
+        isDia = dia;
+    }
 
     public void addAumento(){
         if (!isDia){
                 setSalario(getSalario()* 1.2);
-
         }
-
     }
-
 }
