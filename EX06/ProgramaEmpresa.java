3a27aebd5b4e25a9cde99cb00715d206d18ad1ad
Alisson Silva <78627552+alissonsilvaa@users.noreply.github.com>
2023-12-05 18:15:16 -0300

    Update ProgramaEmpresa.java

--- EX06/ProgramaEmpresa.java
+++ EX06/ProgramaEmpresa.java
@@ -1,20 +1,41 @@
 package EX06;
 
 public class ProgramaEmpresa {
-    public static void main(String[] args) {
-        Administrativo adm = new Administrativo();
-        Tecnico tec = new Tecnico();
-        adm.setNome("Alisson Silva");
-        adm.setSalario(25000);
-        adm.setAddAumento(500);
-        //System.out.println(adm.getGanhoAnual());
-        System.out.println(adm);
 
-        tec.setNome("Maria");
-        tec.setSalario(10000);
-        tec.setAddAumento(500);
-        System.out.println(tec);
-        //System.out.println(tec.getGanhoAnual());
+    public static void main(String[] args) {
+        Administrativo administrativo = new Administrativo();
+        administrativo.setNome("Leandro");
+        administrativo.setSalario(25000);
+        administrativo.setNumeroDeMatricula(123);
+        administrativo.setDia(false);
+
+        System.out.println(administrativo);
+
+        administrativo.addAumento();
+
+        System.out.println(administrativo);
+
+        Tecnico tecnico = new Tecnico();
+        tecnico.setNome("Gustavo");
+        tecnico.setSalario(3500);
+        tecnico.setNumeroDeMatricula(321);
+        System.out.println();
+        System.out.println(tecnico);
+        tecnico.addAumento(50);
+        System.out.println(tecnico);
+
+        Assistente assistente = new Assistente();
+        assistente.setNome("Maria");
+        assistente.setSalario(7000);
+        assistente.setNumeroDeMatricula(555);
+        System.out.println();
+        System.out.println(assistente);
+        assistente.addAumento(500);
+        System.out.println(assistente);
+
+
 
     }
+
+
 }
